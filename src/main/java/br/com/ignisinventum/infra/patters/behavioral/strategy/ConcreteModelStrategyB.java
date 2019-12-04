37af6f5a5352e907acf5094b6c99ec1517f35b8f
Cleidson Dias do Nascimento, MSc <cleidson.nascimento@capgemini.com>
2019-12-04 19:18:14 -0200

    Strategy Pattern

---
+++ src/main/java/br/com/ignisinventum/infra/patters/behavioral/strategy/ConcreteModelStrategyB.java
@@ -0,0 +1,20 @@
+package br.com.ignisinventum.infra.patters.behavioral.strategy;
+
+import br.com.ignisinventum.infra.patters.behavioral.strategy.interfaces.Strategy;
+
+/**
+ * The Class ConcreteModelStrategyB.
+ */
+public class ConcreteModelStrategyB implements Strategy<InputModelHandle, OutputModelHandle> {
+
+	/**
+	 * Execute.
+	 *
+	 * @param input the input
+	 * @return the output model handle
+	 */
+	public OutputModelHandle execute(InputModelHandle input) {
+		return null;
+	}
+
+}
---
+++ src/main/java/br/com/ignisinventum/infra/patters/behavioral/strategy/InputModelHandle.java
@@ -0,0 +1,10 @@
+package br.com.ignisinventum.infra.patters.behavioral.strategy;
+
+import br.com.ignisinventum.infra.patters.behavioral.strategy.interfaces.StrategyHandle;
+
+/**
+ * The Class InputModelHandle.
+ */
+public class InputModelHandle implements StrategyHandle {
+
+}

2ff08e7e07d18bac556e6b20eea7dbefff5dca02
cleid <cleid@10.130.150.27>
2019-12-19 16:10:24 -0300

    Fix bugs

--- src/main/java/br/com/ignisinventum/infra/patters/creational/singleton/ModelContext.java
+++ src/main/java/br/com/ignisinventum/infra/patters/creational/singleton/ModelContext.java
@@ -2,15 +2,16 @@ package br.com.ignisinventum.infra.patters.creational.singleton;
 
 import br.com.ignisinventum.infra.patters.creational.singleton.impl.FactorySingleton;
 
+@SuppressWarnings("static-access")
 public class ModelContext {
-	
+
 	private static FactorySingleton factorySingleton;
 
 	public static void main(String[] args) {
 		ObjectSingletonA ObjectSingleton = (ObjectSingletonA) factorySingleton.getInstance(ObjectSingletonA.class);
 		ObjectSingletonA ObjectSingletonOther = (ObjectSingletonA) factorySingleton.getInstance(ObjectSingletonA.class);
 		ObjectSingletonB ObjectSingletonB = (ObjectSingletonB) factorySingleton.getInstance(ObjectSingletonB.class);
-		
+
 		System.out.println(ObjectSingleton.hashCode());
 		System.out.println(ObjectSingletonOther.hashCode());
 		System.out.println(ObjectSingletonB.hashCode());
